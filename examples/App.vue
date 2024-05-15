985d09df64d10728c11f0e3d053d4d9a238ea7f4
maoHangFei <3061148499@qq.com>
2024-05-15 18:23:49 +0800

    Initialize warehouse

---
+++ examples/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app">
+    <demo-main></demo-main>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ examples/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import demo from "../components/demo";
+Vue.use(demo)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
