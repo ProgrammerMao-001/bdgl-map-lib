b64ca9ea473a9d96aa5ecd4d14d3218138bb526e
maoHangFei <3061148499@qq.com>
2024-06-29 22:54:01 +0800

    a

--- examples/views/useBdMapVGL/useBdMapVGL.vue
+++ examples/views/useBdMapVGL/useBdMapVGL.vue
@@ -15,6 +15,7 @@
 
 <script>
 import mapComps from "../../../components/mapComps/mapComps.vue";
+import chongqingPart from "../../assets/chongqing.json";
 
 export default {
   components: { mapComps },
@@ -53,7 +54,44 @@ export default {
         autoSelect: true, // 根据鼠标位置来自动设置选中项
         data: xinjiangPart.default,
         onClick: (event) => {
-          console.log(event, "event123");
+          console.log(event, "xinjiangPart");
+        },
+      });
+
+      var data = require("./../../assets/chongqing.json");
+      var polygons = [];
+      var len = data.length;
+      for (var i = 0; i < len; i++) {
+        var line = data[i];
+        var polygon = [];
+        var pt = [line[1] * 512, line[2] * 512];
+        for (var j = 3; j < line.length; j += 2) {
+          pt[0] += line[j] / 100 / 2;
+          pt[1] += line[j + 1] / 100 / 2;
+          polygon.push([pt[0], pt[1]]);
+        }
+        if (Math.random() > 0.5) {
+          polygons.push({
+            geometry: {
+              type: "Polygon",
+              coordinates: [polygon],
+            },
+            properties: {
+              height: line[0] / 2,
+            },
+          });
+        }
+      }
+
+      this.$refs.mapComps.initShapeLayer({
+        color: "rgba(194, 147, 75, 0.8)", // 柱状图颜色
+        enablePicked: true, // 是否可以拾取
+        selectedIndex: -1, // 选中项
+        selectedColor: "#ee1111", // 选中项颜色
+        autoSelect: true, // 根据鼠标位置来自动设置选中项
+        data: polygons,
+        onClick: (event) => {
+          console.log(event, "chongqingPart");
         },
       });
     },
